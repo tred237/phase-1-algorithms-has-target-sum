f98b7585b44bedda886a89692a356d01e98b2fbe
trevor dewindt <trevor.dewindt@gmail.com>
2022-08-23 19:50:49 -0600

    commit tree e7ad11635cf5c02c28e42919553837ef2bd48fbb

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // 1
+  const targetObj = {};
+
+  // 0(n)
+  for(element of array){
+    // 0(1)
+    if(targetObj[target - element] !== undefined){
+      return true;
+    } else {
+      targetObj[element] = true;
+    }
+  }
+
+  return false;
 }
 
+// console.log(hasTargetSum([2, 2, 3, 3], 4));
+
 /* 
-  Write the Big O time complexity of your function here
+  Time Complexity -> 0(n)
+  Space Complexity -> 0(3n) -> 0(n)
 */
 
 /* 
-  Add your pseudocode here
+  create object to store array elements
+
+  loop through array and check to see if the difference between the target and the array element already exists in the object
+  if true, return true
+  else, add the array element to the object
+
+  return false if nothing returns true
 */
 
 /*
-  Add written explanation of your solution here
+  I chose to add the array elements to an object b/c selecting elements from an object are immediate and take little time to process.
+  Taking the differnence from each object key and the target sum would allow me to search the object for any elements that equal that difference
+
+  This did not work b/c there can be arrays where there are mulitple elements with the same number
+  I then moved if statement to check the objec in the for of loop b/c that would take care of the case where there are arrays wuith multiple elements of the same number
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +55,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 5, 7, 11], 10));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
 }
 
 module.exports = hasTargetSum;
